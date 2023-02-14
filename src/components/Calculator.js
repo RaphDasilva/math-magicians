3e2a1c7a73e17d4aa5465f8da88a197c8b4c04a9
Raphael Okolo <okolonnaemeka2017@gmail.com>
2023-02-14 21:10:36 +0530

    Make Calculator component to be class-based component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,40 +1,48 @@
 import React from 'react';
 
-const Calculator = () => (
-  <div className="calculator">
-    <div className="display">
-      <p>0</p>
-    </div>
-    <div className="keys">
-      <button type="button">AC</button>
-      <button type="button">+/-</button>
-      <button type="button">%</button>
-      <button type="button" className="act">÷</button>
-    </div>
-    <div className="keys">
-      <button type="button">7</button>
-      <button type="button">8</button>
-      <button type="button">9</button>
-      <button type="button" className="act">X</button>
-    </div>
-    <div className="keys">
-      <button type="button">6</button>
-      <button type="button">5</button>
-      <button type="button">4</button>
-      <button type="button" className="act">-</button>
-    </div>
-    <div className="keys">
-      <button type="button">3</button>
-      <button type="button">2</button>
-      <button type="button">1</button>
-      <button type="button" className="act">+</button>
-    </div>
-    <div className="keys">
-      <button type="submit" className="zero">0</button>
-      <button type="button" className="dot">.</button>
-      <button type="button" className="dot act">=</button>
-    </div>
-  </div>
-);
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  render() {
+    return (
+      <div className="calculator">
+        <div className="display">
+          <p>0</p>
+        </div>
+        <div className="keys">
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button type="button" className="act">÷</button>
+        </div>
+        <div className="keys">
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button" className="act">X</button>
+        </div>
+        <div className="keys">
+          <button type="button">6</button>
+          <button type="button">5</button>
+          <button type="button">4</button>
+          <button type="button" className="act">-</button>
+        </div>
+        <div className="keys">
+          <button type="button">3</button>
+          <button type="button">2</button>
+          <button type="button">1</button>
+          <button type="button" className="act">+</button>
+        </div>
+        <div className="keys">
+          <button type="submit" className="zero">0</button>
+          <button type="button" className="dot">.</button>
+          <button type="button" className="dot act">=</button>
+        </div>
+      </div>
+    );
+  }
+}
 export default Calculator;
