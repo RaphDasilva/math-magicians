c334e2a4c4f3be9660be1dd7316ed7ae6321d132
Raphael Okolo <okolonnaemeka2017@gmail.com>
2023-02-14 21:09:44 +0530

    Make the main component, App component a class-based component.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-const App = () => (
-  <div className="app">
-    <Calculator />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Calculator />
+      </div>
+    );
+  }
+}
 
 export default App;
