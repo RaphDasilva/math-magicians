5d6b1587502e2c94477b9604c668c7d2ee5b41a6
Raphael Okolo <okolonnaemeka2017@gmail.com>
2023-02-21 23:43:28 +0530

    Add nav bar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import style from '../Style/Nav.module.css';
 
 const Nav = () => (
-  <header className="nav-bar">
-    <h1 className="Heading"> Math Magicians </h1>
-    <nav className="nav-links">
-      <Link to="/">Home</Link>
-      <Link to="/calculator">Calculator</Link>
-      <Link to="/quote">Quote</Link>
+  <header className={style.header}>
+    <h1> Math Magicians </h1>
+    <nav className={style.nav}>
+      <Link className={style.link} to="/">Home</Link>
+      <Link className={style.link} to="/calculator">Calculator</Link>
+      <Link className={style.link} to="/quote">Quote</Link>
     </nav>
   </header>
 );
