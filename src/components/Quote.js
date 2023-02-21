8d2c006a5130694c7df586bdc0fceeedd8a399b1
Raphael Okolo <okolonnaemeka2017@gmail.com>
2023-02-21 23:44:16 +0530

    Add and style quote component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import style from '../Style/Quote.module.css';
 
 const Quote = () => (
   <>
-    <p className="quotes">
+    <p className={style.quote}>
       Mathematics is not about numbers, equations, computations, or algorithms:
-      it is about understanding. -William Paul Thurston
+      it is about understanding.
+      <span>-William Paul Thurston</span>
     </p>
   </>
 );
