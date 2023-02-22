fbec4ece2b1a250452977be9da0cddb507919ee9
Raphael Okolo <okolonnaemeka2017@gmail.com>
2023-02-22 22:53:30 +0530

    Create unit tests for Home components

---
+++ src/__test__/Home.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import Home from '../components/Home';
+import '@testing-library/jest-dom';
+
+test('renders Welcome to our page!', () => {
+  render(<Home />);
+  const linkElement = screen.getByText(/Welcome to our page!/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Render Heading tag', () => {
+    render(<Home />)
+    const headTag = screen.getByRole('heading', {level: 2});
+    expect(headTag).toBeInTheDocument();
+})
+
+test('expect home component to match snapshot', () => {
+  const view = renderer.create(<Home />).toJSON();
+  expect(view).toMatchSnapshot();
+});
