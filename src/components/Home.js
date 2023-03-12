72095f620a7db8a3b7de04e3bc2809a8a52e3401
Raphael Okolo <okolonnaemeka2017@gmail.com>
2023-03-12 14:17:16 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,16 +5,11 @@ const Home = () => (
   <div className={style.div}>
     <h2> Welcome to our page! </h2>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed
-      vestibulum erat. Nulla sagittis massa at egestas facilisis. Quisque
-      tincidunt interdum vulputate. Sed orci orci, viverra vel tincidunt vel,
-      consectetur eget tortor. Integer pellentesque eros vel ex maximus, quis
-      aliquet nisl tristique. Nulla lobortis sem a placerat maximus. Duis
-      suscipit purus at nunc bibendum, et finibus quam tempor. Donec sed magna
-      suscipit, bibendum nisi faucibus, dictum erat. Aenean varius eros vitae
-      arcu tempus lobortis. Ut et nisl eget purus ornare tincidunt. Aliquam erat
-      volutpat. Suspendisse vitae finibus odio. Suspendisse aliquam risus massa,
-      vel vulputate mauris tempus a.
+      Welcome to our web calculator app! Our easy-to-use calculator is designed to help you quickly perform basic arithmetic operations such as addition, subtraction, multiplication, and division. With its simple interface and accurate calculations, our calculator is perfect for anyone who needs to perform basic math operations on the go.
+
+Whether you're a student studying for exams, a professional working on a project, or just someone who needs to do some quick calculations, our web calculator app has got you covered. With its intuitive design and user-friendly interface, you'll be able to perform calculations with ease and accuracy.
+
+We're constantly working to improve our calculator app and add new features, so if you have any feedback or suggestions, please don't hesitate to let us know. Thank you for choosing our web calculator app and we hope it makes your life a little bit easier!
 
     </p>
   </div>
